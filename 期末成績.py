45e5cb049df9ba5fa22e03eef36308451b2e21d7
susanah1027 <susanah63258@gmail.com>
2014-10-20 22:36:01 +0800

    期末成績

---
+++ 期末成績.py
@@ -0,0 +1,11 @@
+# 期末成績計算
+
+score1 = int(input("enter first test score:"))
+score2 = int(input("enter second test score:"))
+score3 = int(input("enter third test score:"))
+
+score = [score1, score2, score3]
+
+score.sort()
+
+print (score[0]*0.2 + score[1]*0.3 + score[2]*0.5)
